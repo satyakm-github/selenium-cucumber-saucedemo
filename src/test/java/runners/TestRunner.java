acaca6d28010b6254c9be1e1001fc5c8d10be619
Keerthi <keerthi.419@gmail.com>
2025-11-12 19:41:25 +0000

    Updated test suite with the reports folder location

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = { "stepDefinitions", "Utils"},
-        plugin = { "pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+        plugin = { "pretty", "html:src/reports/cucumber-reports.html", "json:src/reports/cucumber.json"},
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
